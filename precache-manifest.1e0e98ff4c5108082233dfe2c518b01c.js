3c5c6aa9e16511177da588be0c57e8bdb0c09898
vignesh222s <iamvigneshsundaram@gmail.com>
2019-11-27 18:04:41 +0530

    Updates

---
+++ precache-manifest.1e0e98ff4c5108082233dfe2c518b01c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a22c6cbda44f4116cbe5f8ebe30f7d9",
+    "url": "/React-Js-Shopping-Cart-Project/index.html"
+  },
+  {
+    "revision": "40b67f98c123fde018dd",
+    "url": "/React-Js-Shopping-Cart-Project/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "b1e3efaa48bf6e26fed6",
+    "url": "/React-Js-Shopping-Cart-Project/static/css/main.c387ae49.chunk.css"
+  },
+  {
+    "revision": "40b67f98c123fde018dd",
+    "url": "/React-Js-Shopping-Cart-Project/static/js/2.70e102f7.chunk.js"
+  },
+  {
+    "revision": "b1e3efaa48bf6e26fed6",
+    "url": "/React-Js-Shopping-Cart-Project/static/js/main.873cef8b.chunk.js"
+  },
+  {
+    "revision": "c3548905406eca2cfbc7",
+    "url": "/React-Js-Shopping-Cart-Project/static/js/runtime-main.38983da1.js"
+  }
+]);
